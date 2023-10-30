9f114ec4f2b35ad06f97a764775367f9994ed4f0
Kolapakar <127402392+Kolapakar@users.noreply.github.com>
2023-10-30 12:23:37 +0530

    By AKBranch

--- Git_Program/src/co/Demo_git/Demo_git1.java
+++ Git_Program/src/co/Demo_git/Demo_git1.java
@@ -2,6 +2,10 @@ package co.Demo_git;
 
 public class Demo_git1 
 {
+	public void m1()
+	{
+		system.out.println("m1 method");
+	}
 	public static void main(String[] args) 
 	{
 		System.out.println("Aakash kolapkar");
